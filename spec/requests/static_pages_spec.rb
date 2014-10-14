549abad40d9818038eb7292421d476fec1f4eb72
brian1987 <pisano.b@gmail.com>
2014-10-13 21:50:25 -0400

    static

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -10,7 +10,32 @@ RSpec.describe "StaticPages", :type => :request do
   
   describe "Home page" do
     it "should have the content 'Welcome'" do visit '/static_pages/home'
-    expect(page).to have_content('Welcome')
+      expect(page).to have_content('Welcome')
+    end
+    it "should have the right title" do visit '/static_pages/home' 
+      expect(page).to have_title('566 Community Blog | Home')
     end
   end
+
+  describe "Help" do
+    it "should have the content 'Help'" do visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+
+    it "should have the right title" do visit '/static_pages/help' 
+      expect(page).to have_title('566 Community Blog | Help')
+    end
+  end
+
+  describe "About Us" do
+    it "should have the content 'About'" do visit '/static_pages/about'
+      expect(page).to have_content('About')
+    end
+
+    it "should have the right title" do visit '/static_pages/about' 
+      expect(page).to have_title('566 Community Blog | About')
+    end
+  end
+
+
 end
