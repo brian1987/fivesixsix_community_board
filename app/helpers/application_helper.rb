57a7b2021b9d8feede39a65229e3b3eb06e3af00
brian1987 <pisano.b@gmail.com>
2014-10-15 13:37:02 -0400

    meeting commit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+# Returns the full title on a per-page basis.
+  def full_title(page_title)
+  base_title = "566 Community Blog"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
 end
+
+
+#Your writing this comment from one of the worst meetings you've been in
